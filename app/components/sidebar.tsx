b41a75d722a1244936a3d22834e12866ade750a7
Keyu shen <114730623+SteinsHimmer@users.noreply.github.com>
2024-11-02 16:36:34 +0800

    up

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -176,6 +176,7 @@ export function SideBar(props: { className?: string }) {
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
           ccChat
         </div>
+        
         <div className={styles["sidebar-sub-title"]}>
 
           your own AI assistant.(now support gpt-4o,gpt4o-mini,gpt-o1,gpt-oi-mini)
